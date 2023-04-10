ed15dbc7fe3d08d8ad0b2f57e436eca2964dbf0d
Ulisses <ulissessuporteti01@gmail.com>
2023-04-10 12:39:10 -0300

    feat: tailwind.config and others files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './style/global.scss'
 
 function App() {
 
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className='text-3xl font-bold text-blue-600 underline'>Hello World!</h1>
     </div>
   )
 }
